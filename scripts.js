393acaf39785964e80011d5cc542851ac76af4ac
Francisco Carmo <franciscoacarmo@gmail.com>
2022-06-14 16:05:53 -0300

    termina a implementação dos requisitos basicos

--- scripts.js
+++ scripts.js
@@ -5,6 +5,40 @@ function comparador() {
   return Math.random() - 0.5;
 }
 
+// Vira a carta
+function flip(card) {
+  card.children[0].classList.add("flipped");
+  card.children[1].classList.add("flipped");
+
+  rounds++;
+}
+
+// Desvira as cartas que estavam flipped
+function unflipCards() {
+  let flippedFaces = document.querySelectorAll(".flipped");
+  for (const face of flippedFaces) {
+    face.classList.remove("flipped");
+    face.classList.remove("flipped");
+  }
+}
+
+// Passa as frentes que estao flipped para correct
+function correctCards() {
+  let flippedFaces = document.querySelectorAll(".flipped");
+  for (const face of flippedFaces) {
+    face.classList.add("correct");
+    face.classList.add("correct");
+  }
+}
+
+// Analisa se o jogador ganhou
+function didPlayerWin() {
+  correctFronts = document.querySelectorAll(".correct");
+  if (correctFronts.length == cardNumber * 2) {
+    alert(`Você ganhou em ${rounds} jogadas!`);
+  }
+}
+
 // CÓDIGO
 
 let cardNumber = 0;
@@ -57,4 +91,40 @@ for (let i = 0; i < cardNumber; i++) {
   let faceDown = cards[i].querySelector(".back-face");
   img.src = selectedGifs[i];
   faceDown.appendChild(img);
+}
+
+let rounds = 0;
+
+//Analisa se as cartas irao virar ou nao
+function processSelectedCard() {
+  let card = this;
+  if (document.querySelector(".flipped") === null) {
+    flip(card);
+  } else if (
+    card.children[0].classList.contains("flipped") ||
+    card.children[0].classList.contains("correct")
+  ) {
+    console.log("Clicou repetido"); // Não faz nada pq clicou na mesma carta
+  } else {
+    let firstCard = document.querySelector(".flipped");
+    let firstCardImg = firstCard.nextElementSibling.firstChild.src;
+    let secondCardImg = card.children[0].nextElementSibling.firstChild.src;
+
+    if (firstCardImg == secondCardImg) {
+      flip(card);
+
+      correctCards();
+      unflipCards();
+
+      setTimeout(didPlayerWin, 500);
+    } else {
+      flip(card);
+      setTimeout(unflipCards, 1000);
+    }
+  }
+}
+
+// virar a carta
+for (const card of cards) {
+  card.addEventListener("click", processSelectedCard);
 }
